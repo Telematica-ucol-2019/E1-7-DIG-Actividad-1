110059c138e5fa2aff96445019e9c61e783916d5
IvanGodinez21 <ivano.godinez@gmail.com>
2022-11-02 14:07:57 -0600

    All pages are centered in layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="flex flex-col min-h-screen">
+  <div class="flex flex-col h-screen justify-between">
     <Navbar class="sticky top-0 z-10" company="Brand" />
-    <NuxtPage
-      class="min-h-screen bg-light-cherry-blossom-pink dark:bg-dark-dark-siena-3"
-    />
+    <NuxtPage class="bg-light-cherry-blossom-pink dark:bg-dark-dark-siena-3" />
     <Footer company="Brand" />
   </div>
 </template>
